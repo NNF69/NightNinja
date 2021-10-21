d01af92b2b0ea205d25861fbb79a07289f8a0d12
hash <91375482+NNF69@users.noreply.github.com>
2021-10-21 23:00:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from gameData import NightNinja # import NightNinja form GameDate/__init__.py
+
+app = NightNinja()              # initialize app instance from __init__.py
+
+if __name__ == '__main__':
+    app.run()                   # run app
